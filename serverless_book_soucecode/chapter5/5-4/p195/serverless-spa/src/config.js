38c28a169946d5ab5b8267e3db4478f4fbccff87
hiroakiashizawa <jo.ultim.layco@gmail.com>
2021-08-05 10:21:08 +0900

    add official support file

---
+++ serverless_book_soucecode/chapter5/5-4/p195/serverless-spa/src/config.js
@@ -0,0 +1,7 @@
+export default {
+    Region: "ap-northeast-1",
+    ApiBaseUrl: "https://1234abcdef.execute-api.ap-northeast-1.amazonaws.com/Prod",
+    ImageBaseUrl: "http://serverless-app-photos.s3-website-apnortheast-1.amazonaws.com",
+    UserPoolId: "",
+    UserPoolClientId: ""
+}
